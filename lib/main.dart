5b4ee8be5f414afc7786a8cf2fb544598804d20a
鈴木輝 <akira9238pg@gmail.com>
2025-04-14 17:48:34 +0900

    Add chat page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async {
+// void main() {
+//   runApp(const MyApp());
+// }
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -15,12 +18,14 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -37,46 +42,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   String name = '';
   String room = '';
 
+  // エラー表示
   void showError(String message) {
     showDialog(
       context: context,
-      builder: (context) => AlertDialog(content: Text(message))
+      builder: (context) {
+        return AlertDialog(
+          content: Text(message),
+        );
+      },
     );
-
   }
 
+  // 入室処理
   void enter() {
     if (name.isEmpty) {
-      showError('名前を入力してください');
+      showError('あなたの名前を入力してください。');
       return;
     }
     if (room.isEmpty) {
-      showError('ルーム名を入力してください');
+      showError('部屋名を入力してください。');
       return;
     }
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => ChatPage(name: name, room: room)),
+      MaterialPageRoute(
+        builder: (context) {
+          return ChatPage(name: name, room: room);
+        },
+      ),
     );
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat'),
+        title: Text('チャット'),
       ),
       body: ListView(
         children: [
           ListTile(
             title: TextField(
-              decoration: InputDecoration(hintText: '名前'),
+              decoration: InputDecoration(hintText: 'あなたの名前'),
               onChanged: (value) {
                 name = value;
               },
@@ -84,7 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           ListTile(
             title: TextField(
-              decoration: InputDecoration(hintText: 'ルーム名'),
+              decoration: InputDecoration(hintText: '部屋名'),
               onChanged: (value) {
                 room = value;
               },
@@ -92,8 +103,10 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           ListTile(
             title: ElevatedButton(
-              onPressed: () { enter(); },
-              child: Text('入室'),
+              onPressed: () {
+                enter();
+              },
+              child: Text('入室する'),
             ),
           ),
         ],
@@ -104,21 +117,127 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class ChatPage extends StatefulWidget {
   ChatPage({required this.name, required this.room, super.key});
-  
-  final String name;
-  final String room;
+
+  String name;
+  String room;
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<Item> items = [];
+  late FirebaseFirestore firestore;
+  late CollectionReference<Map<String, dynamic>> collection;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    firestore = FirebaseFirestore.instance;
+    collection =
+        firestore.collection('rooms').doc(widget.room).collection('items');
+    watch();
+  }
+
+  // データ更新監視
+  Future<void> watch() async {
+    collection.snapshots().listen((event) {
+      if (mounted) {
+        setState(() {
+          items = event.docs.reversed
+              .map(
+                (document) => Item.fromSnapshot(
+                  document.id,
+                  document.data(),
+                ),
+              )
+              .toList(growable: false);
+        });
+      }
+    });
+  }
+
+  // 保存する
+  Future<void> save() async {
+    final now = DateTime.now();
+    await collection.doc(now.millisecondsSinceEpoch.toString()).set({
+      "date": Timestamp.fromDate(now),
+      "name": widget.name,
+      "text": textEditingController.text,
+    });
+    textEditingController.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.room),
+      appBar: AppBar(title: Text(widget.room)),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                final isMe = item.name == widget.name;
+                return Padding(
+                  padding: isMe
+                      ? EdgeInsets.only(left: 80, right: 16, top: 16)
+                      : EdgeInsets.only(left: 16, right: 80, top: 16),
+                  child: ListTile(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    tileColor: isMe ? Colors.tealAccent : Colors.black12,
+                    subtitle: Text(
+                      // item.name,
+                      '${item.name} ${item.date.toString().replaceAll('-', '/').substring(0, 16)}',
+                    ),
+                    title: Text(item.text),
+                  ),
+                );
+              },
+              itemCount: items.length,
+            ),
+          ),
+          SafeArea(
+            child: ListTile(
+              title: TextField(
+                controller: textEditingController,
+              ),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  save();
+                },
+                child: Text('送信'),
+              ),
+            ),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class Item {
+  const Item({
+    required this.id,
+    required this.name,
+    required this.text,
+    required this.date,
+  });
+
+  final String id;
+  final String name;
+  final String text;
+  final DateTime date;
+
+  factory Item.fromSnapshot(String id, Map<String, dynamic> document) {
+    return Item(
+      id: id,
+      name: document['name'].toString() ?? '',
+      text: document['text'].toString() ?? '',
+      date: (document['date'] as Timestamp?)?.toDate() ?? DateTime.now(),
     );
   }
 }
